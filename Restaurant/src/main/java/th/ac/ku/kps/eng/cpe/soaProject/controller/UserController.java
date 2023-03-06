cb81e41fec438da85cfcfc3be691fe48c62d2422
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-03-06 15:40:48 +0700

    spring boot with HTML

--- Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/controller/UserController.java
+++ Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/controller/UserController.java
@@ -3,28 +3,37 @@ package th.ac.ku.kps.eng.cpe.soaProject.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import th.ac.ku.kps.eng.cpe.soaProject.model.User;
 import th.ac.ku.kps.eng.cpe.soaProject.service.UserService;
 
-@RestController
+@Controller
 @RequestMapping("api/v1/users")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
-	@GetMapping("/index")
-	public String index() {
-		return "Hello World | SQL | hbm.xml | Respository";
-	}
-	
-	@GetMapping("/allusers")
+	@GetMapping("/")
+	@ResponseBody
 	public List<User> getUsers() {
 		return (List<User>)userService.getUsers();
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/home")
+	public String home() {
+		return "pageWithOutUser/home";
 	}
 
 }
--- Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/repository/UserRepository.java
+++ Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/repository/UserRepository.java
@@ -1,11 +1,12 @@
 package th.ac.ku.kps.eng.cpe.soaProject.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import th.ac.ku.kps.eng.cpe.soaProject.model.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer>{
 
 }
