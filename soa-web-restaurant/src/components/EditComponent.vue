6ba20056eccb2a973593554e6af944e5f3f72265
autsadawut.kl <autsadawut.dew10@gmail.com>
2023-03-25 11:04:53 +0700

    update web-api-create-showdata

---
+++ soa-web-restaurant/src/components/EditComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row jsutify-content-center">
+        <div class="col-md-6">
+            <!-- Update Content -->
+            <h1>Edit User</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
---
+++ soa-web-restaurant/src/router/index.js
@@ -0,0 +1,31 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+//components
+import CreateComponent from "@/components/CreateComponent.vue";
+import EditComponent from "@/components/EditComponent.vue";
+import ListComponent from "@/components/ListComponent.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: CreateComponent,
+  },
+  {
+    path: "/edit",
+    name: "edit",
+    component: EditComponent,
+  },
+  {
+    path: "/view",
+    name: "view",
+    component: ListComponent,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
