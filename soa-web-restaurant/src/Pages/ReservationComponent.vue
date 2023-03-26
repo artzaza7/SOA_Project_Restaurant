6c41c6c00d94e7e4459f172818ac418f2288e93b
6320502479 <pattrapol.jam@gmail.com>
2023-03-27 00:35:15 +0700

    pulldataHomeAndReservation

--- soa-web-restaurant/src/Pages/ReservationComponent.vue
+++ soa-web-restaurant/src/Pages/ReservationComponent.vue
@@ -3,21 +3,20 @@
     
     <div class="col"></div>
     <div class="col">
-      <h4 class="mb-3">Reservation_id : {{ user.reservations }}</h4>
-    <h4 class="mb-3">User_id : Variable</h4>
+      <h4 class="mb-3">Reservation_id : {{ user.userId }}</h4>
+    <h4 class="mb-3">User_Name : {{ user.userFirstname }} {{ user.userLastname }}</h4>
       <h4 class="mb-3">Select Table</h4>
       <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-        <option selected>Select Table</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+
+        <!-- <option selected>Select Table</option> -->
+        <option value="1" v-for="table in table" v-bind:key="table.tableRestaurantId">{{ table.tableRestaurantCode }}</option>
       </select>
       <h4>Select Course</h4>
       <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-        <option selected>Select Course</option>
-        <option value="1">One</option>
-        <option value="2">Two</option>
-        <option value="3">Three</option>
+        <!-- <option selected>Select Course</option> -->
+        <option value="1" v-for="coursemenu in course" v-bind:key="coursemenu.courseMenuId">{{ coursemenu.courseMenuName
+ }}</option>
+        
       </select>
       <div class="row justify-content-between">
         <div class="col-4 ">
@@ -35,17 +34,31 @@
 <script>
 import axios from 'axios';
 export default {
+  
     data() {
         return {
-            info: null
+            info: null,
+            table:null,
+            course:null
         }
     },
     mounted() {
         axios
             .get('http://localhost:8081/api/v1/users')
             .then(response => {(this.info = response.data);
+            console.log(response.data)}),
+
+          axios
+            .get('http://localhost:8081/api/v1/tables')
+            .then(response => {(this.table = response.data);
             console.log(response.data)})
 
+            axios
+            .get('http://localhost:8081/api/v1/coursemenus')
+            .then(response => {(this.course = response.data);
+            console.log(response.data)})
+            
     }
+   
 }
 </script>
