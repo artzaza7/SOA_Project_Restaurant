c1d075ddcb08e6e124ffcb74037a4381f2dfdfbf
นายโรจนากร แย้มบางยาง <rojjanakorn.y@ku.th>
2023-04-07 03:03:14 +0700

    Last deploy

--- Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/Restaurant/RestaurantApplication.java
+++ Restaurant/src/main/java/th/ac/ku/kps/eng/cpe/soaProject/Restaurant/RestaurantApplication.java
@@ -2,6 +2,8 @@ package th.ac.ku.kps.eng.cpe.soaProject.Restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan("th.ac.ku.kps.eng.cpe.soaProject")
 
 @EnableJpaRepositories("th.ac.ku.kps.eng.cpe.soaProject.repository")
-public class RestaurantApplication {
+public class RestaurantApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
 
+	@Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder builder) {
+        return     builder.sources(RestaurantApplication.class);
+    }
 }
